caac33cc1e1012e90a70755645d8c69333dca4a9
Rumus Bajracharya <rumus.3353@gmail.com>
2022-03-13 13:44:35 +0545

    react-redux and action creators

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Macarena", duration: "5:05" },
+    { title: "Smells likes teen spirit", duration: "3:05" },
+    { title: "I want it that way", duration: "2:50" },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
