fc09ab0075fd90b5a5f5a9fd2e4c585c2fca8ba6
Rumus Bajracharya <rumus.3353@gmail.com>
2022-03-23 13:50:13 +0545

    redux chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,15 @@
-import { memoize } from "lodash";
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value(); //need to add value to execute the chain command
+};
 
 export const fetchPosts = () => async (dispatch) => {
   const { data } = await jsonPlaceholder.get("posts");
@@ -7,12 +17,16 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: data });
 };
 
-export const fetchUser = (id) => (dispatch) => {
-  _fetchUser(id, dispatch);
-};
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
 
-const _fetchUser = memoize(async (id, dispatch) => {
+// const _fetchUser = memoize(async (id, dispatch) => {
+//   const { data } = await jsonPlaceholder.get(`users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: data });
+// });
+
+export const fetchUser = (id) => async (dispatch) => {
   const { data } = await jsonPlaceholder.get(`users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: data });
-});
+};
